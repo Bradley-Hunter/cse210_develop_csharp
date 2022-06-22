13280b45686b86e3d91036be0cba499902a481a3
McKay Beckstead <bec21017@byui.edu>
2022-06-22 09:18:49 -0600

    draw actors

--- unit05-cycle/Game/Scripting/DrawActorsAction.cs
+++ unit05-cycle/Game/Scripting/DrawActorsAction.cs
@@ -24,15 +24,40 @@ namespace Unit05.Game.Scripting
         /// <inheritdoc/>
         public void Execute(Cast cast, Script script)
         {
-            Cycle snake = (Cycle)cast.GetFirstActor("cycles");
-            List<Actor> segments = snake.GetSegments();
-            Actor score = cast.GetFirstActor("score");
+            // List<Actor> snakes = (Actor)cast.GetActors("cycles");
+
+            Cycle cycle1 = (Cycle)cast.GetFirstActor("cycles");
+            Cycle cycle2 = (Cycle)cast.GetSecondActor("cycles");
+
+            List<Cycle> cycles = new List<Cycle>();
+            cycles.Add(cycle1);
+            cycles.Add(cycle2);
+
+            foreach(Cycle cycle in cycles)
+            {
+            List<Actor> segments = cycle.GetSegments();
+            videoService.DrawActors(segments);
+            }
+
+            Actor score1 = cast.GetFirstActor("score");
+            Actor score2 = cast.GetSecondActor("score");
+
+            List<Actor> scores = new List<Actor>();
+            scores.Add(score1);
+            scores.Add(score2);
+
+            foreach(Score score in scores)
+            {
+            videoService.DrawActor(score);
+            
+            }
+
+
+
             Actor food = cast.GetFirstActor("food");
             List<Actor> messages = cast.GetActors("messages");
             
             videoService.ClearBuffer();
-            videoService.DrawActors(segments);
-            videoService.DrawActor(score);
             videoService.DrawActor(food);
             videoService.DrawActors(messages);
             videoService.FlushBuffer();
